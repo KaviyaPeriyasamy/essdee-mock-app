3a17f202b1555106bdf31cbc651bb489cd003742
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-01 15:51:37 +0530

    new components added

---
+++ src/app/newgrn/newgrn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newgrn',
+  templateUrl: './newgrn.component.html',
+  styleUrls: ['./newgrn.component.css']
+})
+export class NewgrnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
